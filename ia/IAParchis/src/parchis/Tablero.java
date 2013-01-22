cf83007e38d000efeba97a5f2316111ebc77c008
Itziar <itz.agirre@gmail.com>
2013-01-22 19:43:09 +0100

    se pinta el tablero

--- ia/IAParchis/src/parchis/Tablero.java
+++ ia/IAParchis/src/parchis/Tablero.java
@@ -1,10 +1,29 @@
 package parchis;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Tablero extends JApplet {
 	private Casillero casillero;
 	private Jugador jugador;
+	JPanel principal;
+	JPanel fichaPanel;
+	JFrame frame;
+	BufferedImage img = null;
+	Graphics g;
 	
 	private static final long serialVersionUID = 1L;
 	public Casillero getCasillero() {
@@ -19,5 +38,46 @@ public class Tablero extends JApplet {
 	public void setJugador(Jugador jugador) {
 		this.jugador = jugador;
 	}
+	
+	public void init(){
+		principal = new JPanel();
+		fichaPanel = new JPanel();
+		ImageLabel label = new ImageLabel(new ImageIcon("D:/PFC/pictures/tablero.png"));
+		ImageLabel ficha = new ImageLabel(new ImageIcon("D:/PFC/pictures/ficha_roja.png"));
+		ficha.setLocation(29, 37);
+		label.setLocation(29, 37);
+		principal.add(label);
+		fichaPanel.add(ficha);
+		principal.add(fichaPanel);
+		
+		frame = new JFrame();
+		frame.getContentPane().add(principal);
+		frame.pack();
+		frame.setVisible(true);
+		//p = new Panel();
+		
+		//JLabel picLabel = new JLabel(new ImageIcon(img));
+		//principal.add(picLabel);
+		//principal.repaint(); 
+		//p.setBackground(Color.black);
+		//p.setLayout(new GridLayout(3,3,10,10));
+	}
+	
+	 public void ImagePanel() {
+		 try
+			{
+			    img = ImageIO.read(new File("Tablero_parchis.png"));
+			}
+			catch (IOException e)
+			{
+			}
+	    }
+	
+	@Override
+	public void paintComponents(Graphics g) {
+        super.paintComponents(g);
+        g.drawImage(img, 0, 0, null); // see javadoc for more info on the parameters            
+    }
+
 
 }
